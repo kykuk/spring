5de87f07a811d1204cb703f030bcf916d82ea9f4
kykuk <49271055+kykuk@users.noreply.github.com>
2021-11-23 10:40:23 +0900

    Update movie-transform.groovy

--- movie-transform.groovy
+++ movie-transform.groovy
@@ -4,6 +4,24 @@ import groovy.json.JsonOutput
 def jsonSlurper = new JsonSlurper()
 def movie = jsonSlurper.parseText(new String(payload))
 
-println movie
+def connection = new URL( "https://imdb8.p.rapidapi.com/title/get-ratings?tconst=${movie.id}")
+                 .openConnection() as HttpURLConnection
+
+connection.setRequestProperty( 'x-rapidapi-host', 'imdb8.p.rapidapi.com' )
+connection.setRequestProperty( 'x-rapidapi-key', 'f5db1bd94dmsh3e95994a440123ap12f61djsna14efa81e0f2')  
+connection.setRequestProperty( 'Accept', 'application/json' )
+connection.setRequestProperty( 'Content-Type', 'application/json')
+
+if ( connection.responseCode == 200 ) {
+    
+    def imdb = connection.inputStream.withCloseable { inStream ->
+        new JsonSlurper().parse( inStream as InputStream )
+    }
+    
+    movie.imdb = [ "rating": imdb.rating, "ratingCount": imdb.ratingCount ]    
+
+} else {
+    println connection.responseCode + ": " + connection.inputStream.text
+}
 
 JsonOutput.toJson(movie)
