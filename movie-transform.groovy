5f943ffeafb094963b6477187497651b4c325955
kykuk <49271055+kykuk@users.noreply.github.com>
2021-11-23 11:41:35 +0900

    Update movie-transform.groovy

--- movie-transform.groovy
+++ movie-transform.groovy
@@ -6,10 +6,10 @@ def movie = jsonSlurper.parseText(new String(payload))
 
 println movie
 
-def connection = new URL( "https://imdb8.p.rapidapi.com/title/get-ratings?tconst=${movie.id}")
+def connection = new URL( "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/${movie.id}")
                  .openConnection() as HttpURLConnection
 
-connection.setRequestProperty( 'x-rapidapi-host', 'imdb8.p.rapidapi.com' )
+connection.setRequestProperty( 'x-rapidapi-host', 'imdb-internet-movie-database-unofficial.p.rapidapi.com' )
 connection.setRequestProperty( 'x-rapidapi-key', 'f5db1bd94dmsh3e95994a440123ap12f61djsna14efa81e0f2')  
 connection.setRequestProperty( 'Accept', 'application/json' )
 connection.setRequestProperty( 'Content-Type', 'application/json')
@@ -20,12 +20,12 @@ if ( connection.responseCode == 200 ) {
         new JsonSlurper().parse( inStream as InputStream )
     }
     
-    movie.imdb = [ "rating": imdb.rating, "ratingCount": imdb.ratingCount ]    
+    movie.imdb = [ "rating": imdb.rating, "ratingCount": imdb.rating_votes ]    
 
 } else {
     println connection.responseCode + ": " + connection.inputStream.text
 }
 
-println movie
+println "[AF]" + movie
 
 JsonOutput.toJson(movie)
